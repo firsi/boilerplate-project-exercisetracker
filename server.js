eb928c13ef9209a68f165d09bac61e2363b1001f
Alex <43517259+firsi@users.noreply.github.com>
2019-06-07 18:19:35 +0000

    exercise tracker

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+process.env.MONGOLAB_URI = "mongodb+srv://firsi:jeNLrdjaimgthaEu@cluster0-tjplu.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true })
 
 app.use(cors())
 
@@ -18,11 +19,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
@@ -42,7 +38,88 @@ app.use((err, req, res, next) => {
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+// create schema and model
+var Schema= mongoose.Schema;
+var userSchema = new Schema({username: String});
+var exerciseSchema = new Schema({
+                userId: {type: Schema.Types.ObjectId, required: true},
+                description:{type: String, required: true},
+                duration: {type: Number, required: true} ,
+                date: Date
+});
+var User = mongoose.model('User', userSchema);
+var Exercise = mongoose.model('Exercise', exerciseSchema);
 
+//Create a new user
+app.post('/api/exercise/new-user', (req, res) => {
+    
+  User.findOne({username:req.body.username}, (error, username) => {
+    if(error)return console.log(error);
+    
+    // if username doesn't exist insert it else display an error message
+    if(username === null){
+        User.create({username: req.body.username}, (error, username) => {
+          if(error)return console.log(error);
+          
+          res.json(username);
+        })
+    }
+    else{
+      res.send("This username already exists, try another one");
+    }
+  })
+      
+})
+
+// add exercise
+app.post('/api/exercise/add', (req, res) => {
+    User.findOne({_id: req.body.userId}, (error, user) => {
+      if(error) return console.log(error);
+      console.log(user);
+      //if user exists we can add exercise to the database
+      if(user != null){
+          Exercise.create({
+            userId : req.body.userId,
+            description : req.body.description,
+            duration : req.body.duration,
+            date: req.body.date
+          }, (error, exercise) => {
+            if(error)return console.log(error);
+              res.json(exercise);
+          });
+          
+      }
+      else{
+        res.send({error:"The userId you entered doesn't exist"});
+      }
+    })
+})
+
+//show the results
+app.get('/api/exercise/log',(req, res) => {
+  var userId = req.query.userId;
+  console.log(userId)
+  User.findOne({_id: userId},(error, user) => {
+      if(error){
+        res.json({error:"Unknown user"});
+        return console.log(error);
+      
+      }
+      //if user exist we can retrieve information from the database
+    else{
+      Exercise.find({userId: userId})
+      .select('description duration date -_id')
+      .exec((error, exercise) => {
+         res.json({
+           _id:user._id,
+           username:user.username,
+           count:exercise.length,
+           log:exercise
+         }); 
+      })
+    }
+  })
+})
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
